9eb266af8bf3958e28718cd52d008aa23a59e467
Adelina <adelhetman@gmail.com>
2022-07-11 13:56:12 +0300

    added function FormatDate

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = data.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   console.log(response.data);
@@ -10,8 +33,10 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML=`Wind: ${response.data.wind.speed} km/h`;
-  }
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  let currentDayTimeElement = document.querySelector("#currentDayTime");
+  currentDayTimeElement.innerHTML = formatDate(response.data.dt * 1000);
+}
 let apiKey = "1dad91bc92f6c69698e1aad50d0a7304";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
 
