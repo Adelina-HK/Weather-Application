99a93c8ab148d6d922275ebff4dd935272d60b1d
Adelina <adelhetman@gmail.com>
2022-07-15 19:42:40 +0300

    built double API call
    built one API call through another API call for getting forecast for a week.

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate() {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days=["Saturday","Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
   let forecastHTML=`<div class="row">`;
@@ -42,7 +43,15 @@ function displayForecast(){
   });
   forecastHTML=forecastHTML+`</div`;
   forecastElement.innerHTML=forecastHTML;
-  console.log(forecastHTML);
+
+}
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "1dad91bc92f6c69698e1aad50d0a7304";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  
+console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -65,6 +74,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   mainPicElement.setAttribute("alt", response.data.weather[0].main);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -111,4 +121,3 @@ let celsiusTemperature=null;
 
 
 search("Kyiv");
-displayForecast(); 
